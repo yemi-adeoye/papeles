485e8955c53bd23dcde965a8dd5fbc8da119af66
Adeyemi Adeoye <adeyemi_adeoye@hotmail.com>
2021-12-26 15:18:02 +0100

    changes cors origin

--- papeles/settings.py
+++ papeles/settings.py
@@ -151,7 +151,7 @@ STATIC_URL = '/static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ORIGIN_WHITELIST = [
-    'http://localhost:3000'
+    'https://yemi-adeoye.github.io/'
 ]
 
 CORS_ALLOW_CREDENTIALS = True
